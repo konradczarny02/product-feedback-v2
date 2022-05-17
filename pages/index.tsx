986d97d90d27512b2a1fac998c2e4792eddb408b
Konrad Czarny <konradcz02@gmail.com>
2022-05-17 17:04:31 +0200

    initial setup

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const H1 = styled.h1`
+  color: red;
+  font-size: 55px;
+`;
+
+const Home = () => {
+  return <H1>Hello</H1>;
+};
+
+export default Home;
