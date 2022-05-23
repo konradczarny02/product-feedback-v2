daf1d0f192103ea4057c34ce881524c33566afad
Konrad Czarny <konradcz02@gmail.com>
2022-05-23 17:16:19 +0200

    create sign in form with validation and error handling

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -3,11 +3,105 @@ import styled from 'styled-components';
 import { auth } from '../lib/mutations';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
+import { StyledError } from '../components/organisms/Forms/CreateForm.styles';
 
-export const Wrapper = styled.div`
-  width: 100%;
+export const Wrapper = styled.div<{ errors: any; authError: string }>`
+  width: 90%;
+  margin: 50px auto;
+  padding: 5%;
   height: 100%;
+  border-radius: 10px;
   background-color: ${({ theme }) => theme.colors.white};
+
+  h1 {
+    font-size: 18px;
+    font-weight: 700;
+    color: ${({ theme }) => theme.colors.darkBlue};
+    margin: 24px 0;
+  }
+
+  label {
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    font-size: 13px;
+    font-weight: 700;
+    color: ${({ theme }) => theme.colors.darkBlue};
+
+    & > * {
+      margin: 12px 0;
+    }
+
+    input {
+      flex-basis: 100%;
+      height: 48px;
+      border-radius: 10px;
+      background-color: ${({ theme }) => theme.colors.darkerWhite};
+      border: none;
+      padding: 16px;
+      font-size: 14px;
+      color: ${({ theme }) => theme.colors.darkGray};
+
+      &::placeholder {
+        font-size: 13px;
+        color: ${({ theme }) => theme.colors.gray};
+      }
+
+      &:focus {
+        outline: 1px solid ${({ theme }) => theme.colors.blue};
+      }
+    }
+
+    input[type='email'] {
+      outline: ${({ theme, errors, authError }) => {
+        if (errors.email || authError) {
+          return `1px solid ${theme.colors.error}`;
+        } else {
+          return `none`;
+        }
+      }};
+      &:focus {
+        outline: ${({ theme, errors, authError }) => {
+          if (errors.email || authError) {
+            return `1px solid ${theme.colors.error}`;
+          } else {
+            return `1px solid ${theme.colors.blue}`;
+          }
+        }};
+      }
+    }
+
+    input[type='password'] {
+      outline: ${({ theme, errors, authError }) => {
+        if (errors.password || authError) {
+          return `1px solid ${theme.colors.error}`;
+        } else {
+          return `none`;
+        }
+      }};
+      &:focus {
+        outline: ${({ theme, errors, authError }) => {
+          if (errors.password || authError) {
+            return `1px solid ${theme.colors.error}`;
+          } else {
+            return `1px solid ${theme.colors.blue}`;
+          }
+        }};
+      }
+    }
+  }
+
+  button {
+    border: none;
+    border-radius: 10px;
+    background-color: ${({ theme }) => theme.colors.pink};
+    width: 100%;
+    margin: 24px 0;
+    height: 40px;
+    font-size: 13px;
+    color: ${({ theme }) => theme.colors.white};
+    font-weight: 700;
+  }
 `;
 
 interface FormInterface {
@@ -15,8 +109,10 @@ interface FormInterface {
   password: string;
 }
 
+const errorMessage = `Can't be empty`;
+
 const SignUp = () => {
-  const [error, setError] = useState<string>('');
+  const [authError, setAuthError] = useState<string>('');
   const router = useRouter();
   const {
     register,
@@ -27,7 +123,7 @@ const SignUp = () => {
   const onSubmit: SubmitHandler<FormInterface> = async ({ password, email }) => {
     const res = await auth('signin', { email, password });
     if (res.status >= 400) {
-      setError('Wrong email or password');
+      setAuthError('Wrong email or password');
     } else {
       reset();
       setTimeout(() => {
@@ -36,13 +132,44 @@ const SignUp = () => {
     }
   };
   return (
-    <Wrapper>
+    <Wrapper errors={errors} authError={authError}>
+      <h1>Sign in</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <input type="text" {...register('email', { required: true })} />
-        <input type="password" {...register('password', { required: true })} />
+        <label htmlFor="email">
+          Email
+          <input
+            id="email"
+            placeholder="Type your email here"
+            type="email"
+            {...register('email', { onChange: () => setAuthError(''), required: true })}
+          />
+        </label>
+        {errors.email && (
+          <StyledError margin={-3} padding={12}>
+            {errorMessage}
+          </StyledError>
+        )}
+        <label htmlFor="password">
+          Password
+          <input
+            id="password"
+            placeholder="Type your password here"
+            type="password"
+            {...register('password', { onChange: () => setAuthError(''), required: true })}
+          />
+        </label>
+        {errors.password && (
+          <StyledError margin={-3} padding={12}>
+            {errorMessage}
+          </StyledError>
+        )}
+        {authError && (
+          <StyledError margin={0} padding={0}>
+            {authError}
+          </StyledError>
+        )}
         <button type="submit">Sign In</button>
       </form>
-      {error && <p>{error}</p>}
     </Wrapper>
   );
 };
