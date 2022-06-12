af4fe84a08160205947a112670fc85846c6e9961
Konrad Czarny <konradcz02@gmail.com>
2022-06-13 00:15:10 +0200

    fix type in fetcher

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,4 +1,4 @@
-export default function fetcher(url: string, data = undefined) {
+export default function fetcher(url: string, data: any = undefined) {
   return fetch(`${window.location.origin}/api${url}`, {
     method: data ? 'POST' : 'GET',
     credentials: 'include',
